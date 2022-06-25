47cac871008fb38201d96078b0fff281fa8d5a03
Sam Jandris <samjandris@gmail.com>
2022-06-24 21:53:38 -0400

    beginning of background player without title

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+// import ReactPlayer from 'react-player';
 
 function App() {
+  // document.getElementById('videoYT').click();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <iframe
+        className="yt-video"
+        id="videoYT"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/Qsz5m1nl4jM?controls=0&amp;start=1219&autoplay=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+      {/* <ReactPlayer
+        playing
+        width="100%"
+        height="100%"
+        scale={1.1}
+        muted
+        className="react-player"
+        url="https://youtu.be/lw3pxW8O93o"
+        config={{
+          youtube: {
+            playerVars: {
+              disablekb: 1,
+            },
+          },
+        }}
+      /> */}
     </div>
   );
 }
